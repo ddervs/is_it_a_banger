df8bfda248a6434e39f419e09e894f1cfa92ad68
Danial Dervovic <dannydervovic@hotmail.com>
2017-12-29 00:50:02 +0100

    split_files.sh more sophisticated:         - Goes through subfolders and prunes the end (shortest) clip.         - Also converts to .wav for librosa compatibility.

--- scripts/split_files.sh
+++ scripts/split_files.sh
@@ -1,13 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 
-SEGMENT_TIME=5
-DIR="../data/train/"
+SEGMENT_TIME=5 # in seconds
+DIRS=( "../data/banger/" "../data/not_a_banger/" ) 
 
-
-for FILE in ${DIR}*.mp3
+for DIR in "${DIRS[@]}"
 do
-     echo "Processing ${FILE}"
-     ffmpeg -i "${FILE}" -f segment -segment_time ${SEGMENT_TIME} -c copy "${FILE%.*}"%03d.mp3                
-     rm "${FILE}"
+    for FILE in "${DIR}"*.mp3
+    do
+        echo "Processing ${FILE}"
+        ffmpeg -i "${FILE}" -f segment -segment_time ${SEGMENT_TIME} -c copy "${FILE%.*}"%03d.wav                
+        rm "${FILE}"
+        rm "$(ls -t "${FILE%.*}"*.wav | tail -n 1)" # remove last file so uniform length
+    done
 done
-
